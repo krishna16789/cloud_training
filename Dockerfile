bcd4d7a47c2a4676ac16b5e25fa853c982494cd3
krishna-teja-vemuri <krishna_teja_vemuri@apple.com>
2024-03-14 17:20:58 +0530

    new files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
-FROM python:3.9.2
+#!/bin/bash
+FROM --platform=linux/amd64 python:3.8-slim-buster as build
 
 WORKDIR python-docker
 
--- server.py
+++ server.py
@@ -55,4 +55,4 @@ api.add_resource(User, '/user')
 # driver function 
 if __name__ == '__main__': 
   
-    app.run(debug = True) 
+    app.run(debug = True, host='0.0.0.0') 
